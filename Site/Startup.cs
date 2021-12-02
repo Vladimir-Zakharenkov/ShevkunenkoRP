9fa41fe1e29f06023965f0051ffe1029fcd61805
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-12-02 21:14:44 +0300

    Startup.cs добавляем UseAuthorrization

--- Site/Startup.cs
+++ Site/Startup.cs
@@ -38,6 +38,8 @@ namespace Site
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints => endpoints.MapRazorPages());
 
             //app.UseEndpoints(endpoints =>
