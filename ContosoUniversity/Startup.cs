fa0d3b2e0577c51fa3a3857a6935b979967788f7
Владимир Захаренков <zakharenkov1965@yandex.ru>
2021-12-30 18:30:20 +0300

    Начинаем создавать модель Head

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -1,13 +1,12 @@
+﻿using ContosoUniversity.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ContosoUniversity
 {
@@ -23,7 +22,18 @@ namespace ContosoUniversity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var myMaxModelBindingCollectionSize = Convert.ToInt32(
+                        Configuration["MyMaxModelBindingCollectionSize"] ?? "100");
+
+            services.Configure<MvcOptions>(options =>
+                   options.MaxModelBindingCollectionSize = myMaxModelBindingCollectionSize);
+
             services.AddRazorPages();
+
+            services.AddDbContext<SchoolContext>(options =>
+                  options.UseSqlServer(Configuration.GetConnectionString("SchoolContext")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +42,7 @@ namespace ContosoUniversity
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
             }
             else
             {
---
+++ Site.Models/HeadModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Site.Models
+{
+    class HeadModel
+    {
+        [Key]
+        public Guid HeadId { get; set; }
+
+        [Required]
+        public uint PageId { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Title { get; set; }
+    }
+}
