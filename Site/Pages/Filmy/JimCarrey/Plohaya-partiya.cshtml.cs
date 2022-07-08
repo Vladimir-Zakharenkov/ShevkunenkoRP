17ed4632049ebb54eadd88a9ef7af7fc5aa99ae8
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-07-08 16:15:41 +0300

    08-07-0222

---
+++ Site/Pages/Filmy/JimCarrey/Plohaya-partiya.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Site.Pages.Filmy.JimCarrey
+{
+    [BindProperties(SupportsGet = true)]
+    public class Plohaya_partiyaModel : PageModel
+    {
+        public uint PageNumber { get; set; }
+
+        public string VideoProvider { get; set; }
+
+        public void OnGet()
+        {
+            PageNumber = 239;
+        }
+    }
+}
