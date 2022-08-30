3bef760f168adeb3e877fdd62842e3f42a60069d
Владимир Захаренков <zakharenkov1965@yandex.ru>
2022-08-31 01:23:45 +0300

    31-08-2012

---
+++ Jon-Skeet/CS-in-Depth-3Edition/Chapter01/Part17/Program.cs
@@ -0,0 +1,120 @@
+﻿#region Russian
+
+/*
+
+Каждый поставщик имеет название Name (типа string) и идентификатор
+SupplierID (типа int). Кроме того, к типу Product добавлено свойство SupplierID
+и должным образом скорректирован пример данных. Надо признать, что способ 
+назначения каждому товару поставщика нельзя назвать объектно-ориентированным - он
+намного ближе к тому, как данные будут представлены в базе данных. Это упрощает
+демонстрацию рассматриваемого средства в настоящий момент, но в главе 12 будет 
+показано, что LINQ позволяет использовать также и более естественную модель.
+
+Теперь давайте взглянем на код в листинге 1.16, который выполняет соединение
+примеров товаров с примерами поставщиков (очевидно на основе идентификатора 
+поставщика), применяет к товарам тот же самый фильтр по цене, что и ранее, сортирует по
+названию поставщика, а затем по наименованию товара и, наконец, выводит названия
+поставщика и товара для каждого соответствия. Это весьма приличный объем работы,
+который в ранних версиях С# реализовать было совсем нелегко. Однако в LINQ 
+решение довольно тривиально.
+
+*/
+
+// Листинг 1.16. Соединение, фильтрация, упорядочение и проецирование (С# 3)
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Product
+{
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+    public int SupplierID { get; private set; }
+
+    public Product(string name, decimal price, int supplier)
+    {
+        Name = name;
+        Price = price;
+        SupplierID = supplier;
+    }
+
+    Product()
+    {
+    }
+
+    public static List<Product> GetSampleProducts()
+    {
+        return new List<Product>
+            {
+                new Product { Name="West Side Story", Price = 9.99m, SupplierID=1 },
+                new Product { Name="Assassins", Price=14.99m, SupplierID=2 },
+                new Product { Name="Frogs", Price=13.99m, SupplierID=1 },
+                new Product { Name="Sweeney Todd", Price=10.99m, SupplierID=3}
+            };
+    }
+
+    //public override string ToString()
+    //{
+    //    return string.Format("{0}: {1:C}", Name, Price);
+    //}
+}
+
+public class Supplier
+{
+    public string Name { get; private set; }
+    public int SupplierID { get; private set; }
+
+    Supplier()
+    {
+    }
+
+    public static List<Supplier> GetSampleSuppliers()
+    {
+        return new List<Supplier>
+            {
+                new Supplier { Name="Solely Sondheim", SupplierID=1 },
+                new Supplier { Name="CD-by-CD-by-Sondheim", SupplierID=2 },
+                new Supplier { Name="Barbershop CDs", SupplierID=3 }
+            };
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        //Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        List<Product> products = Product.GetSampleProducts();
+        List<Supplier> suppliers = Supplier.GetSampleSuppliers();
+
+        var filtered = from p in products
+                       join s in suppliers
+                       on p.SupplierID equals s.SupplierID
+                       where p.Price > 10
+                       orderby s.Name, p.Name
+                       select new { SupplierName = s.Name, ProductName = p.Name };
+
+        foreach (var v in filtered)
+        {
+            Console.WriteLine("SupplierName={0} - Product={1}", v.SupplierName, v.ProductName);
+        }
+
+        Console.ReadKey();
+    }
+}
+
+/*
+
+Вы можете обнаружить, что код в листинге 1.16 удивительно похож на SQL. И действительно, 
+первая реакция многих разработчиков на язык LINQ (до его внимательного
+исследования) - его откладывание в сторону как попытки простого встраивания возможностей 
+SQL внутрь языка ради взаимодействия с базами данных. К счастью, хотя
+язык LINQ позаимствовал синтаксис и некоторые идеи из SQL, работа с ним не требует
+наличия базы данных. Показанный до сих пор код вообще не касается базы данных.
+На самом деле можно было бы запрашивать данные из любых источников, например, XML.
+
+*/
+
+#endregion
---
+++ Jon-Skeet/CS-in-Depth-3Edition/Chapter01/Part18/Program.cs
@@ -0,0 +1,79 @@
+﻿#region Russian
+
+/*
+
+1.4.2. Запрашивание файла XML
+
+Предположим, что вместо жесткого кодирования поставщиков и товаров 
+используется следующий файл XML:
+
+<?xml version="1.0"?>
+<Data>
+  <Products>
+    <Product Name="West Side Story" Price="9.99" SupplierID="1" />
+    <Product Name="Assassins" Price="14.99" SupplierID="2" />
+    <Product Name="Frogs" Price="13.99" SupplierID="1" />
+    <Product Name="Sweeney Todd" Price="10.99" SupplierID="3" />
+  </Products>
+
+  <Suppliers>
+    <Supplier Name="Solely Sondheim" SupplierID="1" />
+    <Supplier Name="CD-by-CD-by-Sondheim" SupplierID="2" />
+    <Supplier Name="Barbershop CDs" SupplierID="3" />
+  </Suppliers>
+</Data>
+
+Файл довольно прост, но как лучше всего извлекать из него данные? Каким образом
+его запрашивать? Как выполнять на нем соединение? Несомненно, это должно быть
+сложнее того, что делалось в листинге 1.16, правильно? В листинге 1.17 можно оценить
+объем работы, необходимой для выполнения запроса с помощью LINQ to XML.
+
+*/
+
+
+// Листинг 1.17. Сложная обработка файла XML с помощью LINQ to XML (С# 3)
+
+using System;
+using System.Linq;
+using System.Xml.Linq;
+
+public class Program
+{
+    static void Main()
+    {
+        //string load = "E:\\Jon-Skit-CS-In-Depth-3edition\\Chapter-1\\Part-18\\data.xml";
+        string load = "data.xml";
+
+        XDocument doc = XDocument.Load(load);
+
+        var filtered = from p in doc.Descendants("Product")
+                       join s in doc.Descendants("Supplier")
+                       on (int)p.Attribute("SupplierID") equals (int)s.Attribute("SupplierID")
+                       where (decimal)p.Attribute("Price") > 10
+                       orderby (string)s.Attribute("Name"), (string)p.Attribute("Name")
+                       select new { SupplierName = (string)s.Attribute("Name"), ProductName = (string)p.Attribute("Name") };
+
+        foreach (var v in filtered)
+        {
+            Console.WriteLine("Supplier = {0} - Product = {1}", v.SupplierName, v.ProductName);
+        }
+
+        Console.ReadKey();
+    }
+}
+
+/*
+
+Подход не настолько прямолинеен, т.к. системе необходимо сообщить о том, каким
+образом должны восприниматься данные (в плане того, какие атрибуты в качестве
+каких типов должны применяться), но об этом речь пойдет чуть позже. В частности,
+существует очевидная связь между частями двух листингов. Не будь ограничений на
+длину печатной строки в книге, вы бы легко заметили построчное соответствие между
+этими двумя запросами.
+
+Все еще под впечатлением? Или недостаточно убедительно? Давайте теперь поместим 
+данные туда, где они с большой вероятностью должны находиться - в базу данных.
+
+*/
+
+#endregion
